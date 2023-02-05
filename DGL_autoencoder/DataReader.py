4475cebcb779e043d22a6958e98ccb0bd8cdb13c
niklai99 <lai.nicolo99@gmail.com>
2023-02-05 17:46:24 +0100

    uploading edge computation

---
+++ DGL_autoencoder/DataReader.py
@@ -0,0 +1,116 @@
+import os
+import numpy as np
+import pandas as pd
+import h5py
+
+class DataReader:
+    
+    def __init__(self, path):
+        
+        self.path = path
+        self.data = None
+        
+    def get_filenames(self):
+        
+        fnames = os.listdir(self.path)
+        fnames = [filename for filename in fnames if filename.endswith(".h5")]
+
+        return fnames
+    
+    
+    def read_single_file(self, fname):
+        
+        file_path = os.path.join(self.path, fname)
+        h5_file = h5py.File(file_path, "r")
+        
+        # turn "jets" into a dataframe with column names given by "jetFeaturesNames"
+
+        jet_feature_names = h5_file["jetFeatureNames"][:]
+
+        # remove the b' from the beginning and the ' from the end of each string
+        jet_feature_names = [name.decode("utf-8") for name in jet_feature_names]
+
+        df = pd.DataFrame(h5_file["jets"][:], columns=jet_feature_names)
+
+        # keep features
+        keep = ["j_g", "j_q", "j_w", "j_z", "j_t", "j_undef"]
+
+        # rename features to "isGluon", "isQuark", "isW", "isZ", "isTop", "isUndefined"
+        df = df[keep].rename(columns={"j_g": "isGluon", "j_q": "isQuark", "j_w": "isW", "j_z": "isZ", "j_t": "isTop", "j_undef": "isUndefined"}).astype(int)
+
+
+        # LABELS
+        labels = df.values
+        
+        # FEATURES
+        e      = np.reshape(h5_file["jetConstituentList"][:, :, 3],  (10000, 100)) # E
+        e_rel  = np.reshape(h5_file["jetConstituentList"][:, :, 4],  (10000, 100)) # E
+        pt     = np.reshape(h5_file["jetConstituentList"][:, :, 5],  (10000, 100)) # pT
+        pt_rel = np.reshape(h5_file["jetConstituentList"][:, :, 6],  (10000, 100)) # pT
+        dEta   = np.reshape(h5_file["jetConstituentList"][:, :, 8],  (10000, 100)) # dEta
+        dPhi   = np.reshape(h5_file["jetConstituentList"][:, :, 11], (10000, 100)) # dPhi
+        dR     = np.reshape(h5_file["jetConstituentList"][:, :, 13], (10000, 100)) # dR
+        
+
+        return {"labels": labels,"e": e, "e_rel": e_rel, "pt": pt, "pt_rel": pt_rel, "dEta": dEta, "dPhi": dPhi, "dR": dR}
+
+
+    
+    def read_files(self, n_files=None):
+        
+        fnames = self.get_filenames()
+        
+        if n_files is not None:
+            fnames = fnames[:n_files]
+            
+        for i, fname in enumerate(fnames):
+            
+            print("Reading file", fname)
+            
+            data = self.read_single_file(fname)
+            
+            if i == 0:
+                labels = data["labels"]
+                e      = data["e"]
+                e_rel  = data["e_rel"]
+                pt     = data["pt"]
+                pt_rel = data["pt_rel"]
+                dEta   = data["dEta"]
+                dPhi   = data["dPhi"]
+                dR     = data["dR"]
+                
+            else:
+                labels = np.concatenate((labels, data["labels"]))
+                e      = np.concatenate((e, data["e"]))
+                e_rel  = np.concatenate((e_rel, data["e_rel"]))
+                pt     = np.concatenate((pt, data["pt"]))
+                pt_rel = np.concatenate((pt_rel, data["pt_rel"]))
+                dEta   = np.concatenate((dEta, data["dEta"]))
+                dPhi   = np.concatenate((dPhi, data["dPhi"]))
+                dR     = np.concatenate((dR, data["dR"]))
+    
+        self.data = {"labels": labels,"e": e, "e_rel": e_rel, "pt": pt, "pt_rel": pt_rel, "dEta": dEta, "dPhi": dPhi, "dR": dR}        
+    
+    
+    def get_labels(self):
+        if self.data:
+            return self.data["labels"]
+        else:
+            raise RuntimeError('No data read, please call .read_files before!')
+    
+    def get_features(self):
+        if self.data:
+            return np.stack(
+                (
+                    self.data["dEta"],
+                    self.data["dPhi"],
+                    self.data["e"],
+                    self.data["e_rel"],
+                    self.data["pt"],
+                    self.data["pt_rel"],
+                    self.data["dR"],
+                ),
+                axis=2,
+            ).astype(np.float32)
+        else:
+            raise RuntimeError('No data read, please call .read_files before!')
---
+++ DGL_autoencoder/compute_edges.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+
+
+def compute_dPhi_ij(dPhi_i, dPhi_j):
+    """Compute the difference in azimuthal angle between two particles."""
+    return dPhi_i - dPhi_j
+
+def compute_dEta_ij(dEta_i, dEta_j):
+    """Compute the difference in pseudorapidity between two particles."""
+    return dEta_i - dEta_j
+
+def compute_R_ij(dEta_ij, dPhi_ij):
+    """Compute the distance between two particles in the transverse plane."""
+    return np.sqrt(dEta_ij**2 + dPhi_ij**2)
+
+def compute_m_ij(e_i, e_j, pt_i, pt_j, dPhi_ij):
+    """Compute the invariant mass of two particles."""
+    # invariant mass of two massive particles as a function of the two energies, the two transverse momenta and the angle between them
+    return np.sqrt(2 * e_i * e_j * (1 - np.cos(dPhi_ij))) # CHECK THIS
+
+def node_distance(pt_i, pt_j, r, r_ij, alpha):
+    """Compute the distance between two nodes in the graph."""
+    return np.min((pt_i**(2*alpha), pt_j**(2*alpha))) * r_ij**r
+
+def compute_one_edge_feature(jet, i, j):
+    """Compute the edge feature for one edge."""
+    
+    dEta_ij = compute_dEta_ij(jet[i, 0], jet[j, 0])
+    dPhi_ij = compute_dPhi_ij(jet[i, 1], jet[j, 1])
+    dR_ij   = compute_R_ij(dEta_ij, dPhi_ij)
+    m_ij    = compute_m_ij(jet[i, 2], jet[j, 2], jet[i, 4], jet[j, 4], dPhi_ij)
+
+    # compute the edge feature
+    e_0 =        node_distance(pt_i=jet[i, 4], pt_j=jet[j, 4], r=jet[i, 6], r_ij=dR_ij, alpha=0)
+    e_1 = np.log(node_distance(pt_i=jet[i, 4], pt_j=jet[j, 4], r=jet[i, 6], r_ij=dR_ij, alpha=1))
+    e_2 = np.log(m_ij)
+
+    return np.array([e_0, e_1, e_2])
+    
+
+
+def compute_edge_features(data):
+    # compute the edge features for all the jets in the dataset
+    # final shape of the edges: (n_jets, n_particles * n_particles, 3) 
+
+    # list to store the edge features for all the jets
+    edge_features_all = []
+
+    # loop over all the jets
+    for k in range(data.shape[0]):
+
+        # list to store the edge features for the current jet
+        edge_features = []
+
+        # get the current jet
+        jet = data[k, :, :]
+
+        # loop over all the particles in the jet
+        for i in range(jet.shape[0]):
+            # loop over all the particles in the jet
+            for j in range(jet.shape[0]):
+
+                # if the two particles are the same, the edge feature is just a vector of ones
+                edge_feature = np.ones(3) if i == j else compute_one_edge_feature(jet, i, j)
+                edge_features.append(edge_feature)
+
+        # store the edge features for the current jet
+        edge_features = np.array(edge_features)
+        edge_features_all.append(edge_features)
+
+    return np.array(edge_features_all)
